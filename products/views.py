a175e90f3321fd44a776414ea57adb6ed1e72c05
Roisin Ocon <roconnell11@icloud.com>
2024-06-08 22:09:42 +0000

    Add delete product functionality for admin users

--- products/views.py
+++ products/views.py
@@ -77,9 +77,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -95,7 +95,7 @@ def add_product(request):
 
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    product = get_object_or_404(Product, pk=product_id)  # prefill the form
+    product = get_object_or_404(Product, pk=product_id)  # get product to prefill the form
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
@@ -115,3 +115,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+
+    return redirect(reverse('products'))
