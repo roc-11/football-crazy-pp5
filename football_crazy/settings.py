bde618d6358a69cdf0952e5162c2823b85baa5a5
Roisin Ocon <roconnell11@icloud.com>
2024-06-16 15:27:52 +0000

    Add newsletter app, view, url and templates

--- football_crazy/settings.py
+++ football_crazy/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'profiles',
     'about',
     'contact',
+    'newsletter',
 
     # Other
     'crispy_forms',
---
+++ newsletter/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('send-email/', views.send_newsletter, name='send_email'),
+]
